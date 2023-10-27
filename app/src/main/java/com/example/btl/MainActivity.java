0c762c66596aae03f9bcce2868a9535788880f73
Thanh Hien <hien10082002@gmail.com>
2023-10-27 10:31:03 +0700

    app

--- app/src/main/java/com/example/btl/MainActivity.java
+++ app/src/main/java/com/example/btl/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.btl;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+////        DatabaseReference databaseReference = database.getReference(path: "/" );
+//
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                DataSnapshot dataSnapshot_name = snapshot.child(path: "name");
+//                String name = dataSnapshot_name.getValue(String.class);
+//                TextView nv = (TextView)  findViewById(R.id.nv);
+//                nv.setText(name);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        })
+//    }
 }
